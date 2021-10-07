fd7acda0876c2bae99beaee8517c2ea3804e4377
Sergey <Hardsgams@gmail.com>
2021-10-07 23:57:16 +0500

    task 1 - completed

--- task 1.py
+++ task 1.py
@@ -1,15 +1,6 @@
 import requests
 import pprint
 
-'''Кто самый умный супергерой? 
-Есть API по информации о супер-героях. 
-Нужно определить кто самый умный(intelligence) из трех супер-героев - Hulk, Captain America, Thanos. 
-Для определения id нужно использовать метод /search/name
-Токен, который нужно использовать для доступа к API: 2619421814940190.
-Таким образом, все адреса для доступа к API должны начинаться с https://superheroapi.com/api/2619421814940190/.
-
-https://superheroapi.com/api/2619421814940190/search/name'''
-
 TOKEN = "2619421814940190"
 
 
@@ -38,12 +29,10 @@ def thanos_intelligence():
 
 
 if __name__ == '__main__':
-    # pprint.pprint(hulk_intelligence())
-    # pprint.pprint(captain_america_intelligence())
-    # pprint.pprint(thanos_intelligence())
-
     my_dict = {"Hulk": hulk_intelligence(),
                "Captain America": captain_america_intelligence(),
                "Thanos": thanos_intelligence()}
+
     the_smartest_character = max(my_dict)
+
     print(the_smartest_character)

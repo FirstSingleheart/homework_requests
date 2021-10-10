8701d3d8e1edc50a9454fb3da950bc6b042bed8e
Sergey <Hardsgams@gmail.com>
2021-10-10 19:34:33 +0500

    task 2 added ver. 1.01

--- task 2.py
+++ task 2.py
@@ -14,7 +14,7 @@ class YandexDisk:
             'Authorization': f'OAuth {self.token}'
         }
 
-    def get_upload_link(self, path_to_file):
+    def get_upload_link(self, disk_file_path=None):
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = self.get_headers()
         params = {"path": 'test file.txt', "overwrite": "true"}
@@ -23,7 +23,7 @@ class YandexDisk:
         return response.json()["href"]
 
     def upload_file_to_disk(self, path_to_file):
-        url = f"{self.get_upload_link(path_to_file)}"
+        url = f"{self.get_upload_link()}"
         with open(path_to_file, "rb") as f:
             response = requests.put(url=url, data=f)
         response.raise_for_status()
@@ -39,4 +39,3 @@ if __name__ == '__main__':
 
     ya_disk = YandexDisk(token)
     ya_disk.upload_file_to_disk(path_to_file)
-

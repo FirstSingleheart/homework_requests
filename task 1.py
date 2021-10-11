ccca7be5ef06a4094324b2752848bf3c78039e92
Sergey <Hardsgams@gmail.com>
2021-10-11 23:13:33 +0500

    task 1 ver. 1.02

--- task 1.py
+++ task 1.py
@@ -4,34 +4,19 @@ import pprint
 TOKEN = "2619421814940190"
 
 
-def hulk_intelligence():
-    url = f"https://superheroapi.com/api/{TOKEN}/search/Hulk"
-    params = {"name": "Hulk"}
+def name_intelligence(name):
+    url = f"https://superheroapi.com/api/{TOKEN}/search/{name}"
+    params = {"name": f"{name}"}
     response = requests.get(url=url, params=params, timeout=5)
-    hulk = response.json()
-    return hulk['results'][0]['powerstats']['intelligence']
-
-
-def captain_america_intelligence():
-    url = f"https://superheroapi.com/api/{TOKEN}/search/Captain_America"
-    params = {"name": "Captain America"}
-    response = requests.get(url=url, params=params, timeout=5)
-    captain_america = response.json()
-    return captain_america['results'][0]['powerstats']['intelligence']
-
-
-def thanos_intelligence():
-    url = f"https://superheroapi.com/api/{TOKEN}/search/Thanos"
-    params = {"name": "Thanos"}
-    response = requests.get(url=url, params=params, timeout=5)
-    thanos = response.json()
-    return thanos['results'][0]['powerstats']['intelligence']
+    resp = response.json()
+    return resp['results'][0]['powerstats']['intelligence']
 
 
 if __name__ == '__main__':
-    my_dict = {"Hulk": hulk_intelligence(),
-               "Captain America": captain_america_intelligence(),
-               "Thanos": thanos_intelligence()}
+
+    my_dict = {"Hulk": name_intelligence("Hulk"),
+               "Captain America": name_intelligence("Captain America"),
+               "Thanos": name_intelligence("Thanos")}
 
     the_smartest_character = max(my_dict)
 
